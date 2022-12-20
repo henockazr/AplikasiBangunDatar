d5825ac70a1f658814ca6464c261c9fde45960f6
henockazr <azrielhenock@yahoo.com>
2022-12-20 10:48:41 +0700

    Initiate Commit Project

---
+++ AplikasiBangunDatar/src/main/java/com/mycompany/aplikasibangundatar/BangunDatar.java
@@ -0,0 +1,29 @@
+package com.mycompany.aplikasibangundatar;
+
+/**
+ *
+ * @author User
+ */
+abstract class BangunDatar {
+    private String namaBangunDatar;
+    
+    public BangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    public BangunDatar(){
+    
+    }
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    abstract double luas();
+    abstract double keliling();
+    
+}
